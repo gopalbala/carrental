bdab48bdc1a2d3b3cf00cbd078207a3e281e8289
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-07-26 17:22:47 +0530

    updated UserRepository with filter functions

--- src/main/java/com/gb/carrental/repository/UserRepository.java
+++ src/main/java/com/gb/carrental/repository/UserRepository.java
@@ -1,18 +1,57 @@
 package com.gb.carrental.repository;
 
+import com.gb.carrental.exceptions.AccountDoesNotExistsException;
+import com.gb.carrental.model.account.Account;
 import com.gb.carrental.model.account.User;
+import com.gb.carrental.model.reservation.VehicleReservation;
+import com.gb.carrental.model.vehicle.HireableVehicle;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
-public class UserRepository extends AccountRepository {
+public class UserRepository implements AccountRepository {
     public static Map<String, User> userMap = new HashMap<>();
     public static List<User> users = new ArrayList<>();
 
-    @Override
-    public void resetPassword(String userId, String password) {
-
+    public Account createAccount(Account account) {
+        userMap.putIfAbsent(account.getEmail(), (User) account);
+        return account;
     }
+
+    public void resetPassword(String userId, String password) throws AccountDoesNotExistsException {
+        if (userMap.get(userId) == null)
+            throw new AccountDoesNotExistsException("Account does not exist.");
+        userMap.get(userId).setPassword(password);
+    }
+
+    public List<HireableVehicle> getHiredVehicles(String userId) {
+
+        List<VehicleReservation> vehicleReservationList =
+                VehicleReservationRepository.vehicleReservations
+                        .stream().filter(vehicleReservation ->
+                        vehicleReservation.getUsrId().equalsIgnoreCase(userId))
+                        .collect(Collectors.toList());
+        return vehicleReservationList.stream()
+                .map(vehicleReservation -> vehicleReservation.getVehicle())
+                .collect(Collectors.toList());
+    }
+
+    public List<HireableVehicle> getHiredVehicles(String userId, LocalDateTime startDate,
+                                                  LocalDateTime endDate) {
+        List<VehicleReservation> vehicleReservationList =
+                VehicleReservationRepository.vehicleReservations
+                        .stream().filter(vehicleReservation ->
+                        vehicleReservation.getUsrId().equalsIgnoreCase(userId) &&
+                                vehicleReservation.getReservationDate().isAfter(startDate) &&
+                                vehicleReservation.getReservationDate().isBefore(endDate))
+                        .collect(Collectors.toList());
+        return vehicleReservationList.stream()
+                .map(vehicleReservation -> vehicleReservation.getVehicle())
+                .collect(Collectors.toList());
+    }
+
 }
