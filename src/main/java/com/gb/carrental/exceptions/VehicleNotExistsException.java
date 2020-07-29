a0eaab93cb531b94d858b15d835de4756732a5d4
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-07-29 13:12:15 +0530

    added VehicleNotExistsException

---
+++ src/main/java/com/gb/carrental/exceptions/VehicleNotExistsException.java
@@ -0,0 +1,7 @@
+package com.gb.carrental.exceptions;
+
+public class VehicleNotExistsException extends Exception {
+    public VehicleNotExistsException(String message) {
+        super(message);
+    }
+}
