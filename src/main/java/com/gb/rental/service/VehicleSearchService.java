e105ee9f6dfe4e520d3aed5f447209d1ba9aabe9
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-07-30 20:49:09 +0530

    refactored vehicle search method signatures

--- src/main/java/com/gb/rental/service/VehicleSearchService.java
+++ src/main/java/com/gb/rental/service/VehicleSearchService.java
@@ -1,15 +1,18 @@
 package com.gb.rental.service;
 
 import com.gb.rental.model.vehicle.HireableVehicle;
+import com.gb.rental.model.vehicle.VehicleType;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface VehicleSearchService {
-    List<HireableVehicle> searchByType(String type, LocalDateTime fromDate, LocalDateTime toDate);
+    List<HireableVehicle> searchByType(VehicleType vehicleType, String city,
+                                       LocalDateTime fromDate, LocalDateTime toDate);
 
-    List<HireableVehicle> searchByModel(String make,
+    List<HireableVehicle> searchByModel(String make, String city,
                                         String model, LocalDateTime fromDate, LocalDateTime toDate);
 
-    List<HireableVehicle> searchByType(int seats, LocalDateTime fromDate, LocalDateTime toDate);
+    List<HireableVehicle> searchByType(int seats, String city,
+                                       LocalDateTime fromDate, LocalDateTime toDate);
 }
--- src/main/java/com/gb/rental/service/VehicleSearchServiceImpl.java
+++ src/main/java/com/gb/rental/service/VehicleSearchServiceImpl.java
@@ -1,23 +1,27 @@
 package com.gb.rental.service;
 
 import com.gb.rental.model.vehicle.HireableVehicle;
+import com.gb.rental.model.vehicle.VehicleType;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public class VehicleSearchServiceImpl implements VehicleSearchService {
     @Override
-    public List<HireableVehicle> searchByType(String type, LocalDateTime fromDate, LocalDateTime toDate) {
+    public List<HireableVehicle> searchByType(VehicleType vehicleType, String city,
+                                              LocalDateTime fromDate, LocalDateTime toDate) {
         return null;
     }
 
     @Override
-    public List<HireableVehicle> searchByModel(String make, String model, LocalDateTime fromDate, LocalDateTime toDate) {
+    public List<HireableVehicle> searchByModel(String make, String city,
+                                               String model, LocalDateTime fromDate, LocalDateTime toDate) {
         return null;
     }
 
     @Override
-    public List<HireableVehicle> searchByType(int seats, LocalDateTime fromDate, LocalDateTime toDate) {
+    public List<HireableVehicle> searchByType(int seats, String city,
+                                              LocalDateTime fromDate, LocalDateTime toDate) {
         return null;
     }
 }
