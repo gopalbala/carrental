47a283419ec2f7fcf0e7aca9d93442197ebfc0bb
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-07-31 02:39:50 +0530

    added vehicle service test and updated test data

--- src/test/java/com/gb/rental/TestData.java
+++ src/test/java/com/gb/rental/TestData.java
@@ -2,8 +2,15 @@ package com.gb.rental;
 
 import com.gb.rental.model.common.Address;
 import com.gb.rental.model.common.Coordinates;
+import com.gb.rental.model.reservation.ReservationStatus;
+import com.gb.rental.model.reservation.VehicleInventory;
+import com.gb.rental.model.reservation.VehicleReservation;
+import com.gb.rental.model.reservation.VehicleReservationType;
 import com.gb.rental.model.vehicle.*;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class TestData {
@@ -24,6 +31,7 @@ public class TestData {
         VehicleLocation vehicleLocation = new VehicleLocation();
         vehicleLocation.setAddress(getAddress());
         vehicleLocation.setCoordinates(getCoordinates());
+        truck.setParkedLocation(vehicleLocation);
         return truck;
     }
 
@@ -44,6 +52,28 @@ public class TestData {
         VehicleLocation vehicleLocation = new VehicleLocation();
         vehicleLocation.setAddress(getAddress());
         vehicleLocation.setCoordinates(getCoordinates());
+        car.setParkedLocation(vehicleLocation);
+        return car;
+    }
+
+    public static HireableVehicle getMarutiHatchBack() {
+        HireableVehicle car = new Car();
+        String id = UUID.randomUUID().toString();
+        car.setId(id);
+        car.setLicensePlateNumber("ka02ca7416");
+        car.setQrCode(id);
+        car.setMake("Maruti");
+        car.setMake("Swift");
+        car.setYearOfManufacture(2015);
+        car.setMileage(55010);
+        car.setNumberOfSeats(5);
+        car.setVehicleCategory(VehicleCategory.PASSENGER);
+        car.setVehicleStatus(VehicleStatus.AVAILALBE);
+        car.setVehicleType(VehicleType.HATCHBACK);
+        VehicleLocation vehicleLocation = new VehicleLocation();
+        vehicleLocation.setAddress(getAddress());
+        vehicleLocation.setCoordinates(getCoordinates());
+        car.setParkedLocation(vehicleLocation);
         return car;
     }
 
@@ -64,6 +94,7 @@ public class TestData {
         VehicleLocation vehicleLocation = new VehicleLocation();
         vehicleLocation.setAddress(getAddress());
         vehicleLocation.setCoordinates(getCoordinates());
+        car.setParkedLocation(vehicleLocation);
         return car;
     }
 
@@ -83,5 +114,51 @@ public class TestData {
         coordinates.setLatitude(12.459454);
         coordinates.setLatitude(79.459454);
         return coordinates;
+    }
+
+
+    public static VehicleReservation buildHatchbackReservation() {
+        HireableVehicle vehicle = getHatchBack();
+        vehicle.setVehicleStatus(VehicleStatus.BOOKED);
+        VehicleReservation vehicleReservation = new VehicleReservation();
+        vehicleReservation.setUsrId("user1");
+        vehicleReservation.setReservationId(UUID.randomUUID().toString());
+        vehicleReservation.setFromDate(LocalDateTime.now());
+        vehicleReservation.setDueDate(LocalDateTime.now().plusDays(2));
+        vehicleReservation.setStatus(ReservationStatus.ACTIVE);
+        vehicleReservation.setStartMileage(vehicle.getMileage());
+        vehicleReservation.setPickupLocation(
+                vehicle.getParkedLocation().getAddress());
+
+        vehicleReservation.setAccocatedVehicleId(vehicle.getId());
+        vehicleReservation.setVehicleReservationType(VehicleReservationType.DAY);
+        return vehicleReservation;
+    }
+
+    public static VehicleReservation buildMarutiHatchbackReservation() {
+        HireableVehicle vehicle = getMarutiHatchBack();
+        vehicle.setVehicleStatus(VehicleStatus.BOOKED);
+        VehicleReservation vehicleReservation = new VehicleReservation();
+        vehicleReservation.setUsrId("user1");
+        vehicleReservation.setReservationId(UUID.randomUUID().toString());
+        vehicleReservation.setFromDate(LocalDateTime.now().plusDays(20));
+        vehicleReservation.setDueDate(LocalDateTime.now().plusDays(25));
+        vehicleReservation.setStatus(ReservationStatus.ACTIVE);
+        vehicleReservation.setStartMileage(vehicle.getMileage());
+        vehicleReservation.setPickupLocation(
+                vehicle.getParkedLocation().getAddress());
+
+        vehicleReservation.setAccocatedVehicleId(vehicle.getId());
+        vehicleReservation.setVehicleReservationType(VehicleReservationType.DAY);
+        return vehicleReservation;
+    }
+
+    public static List<VehicleInventory> buildVehicleInventory() {
+        List<VehicleInventory> vehicleInventories = new ArrayList<>();
+        vehicleInventories.add(new VehicleInventory(buildHatchbackReservation(),
+                getHatchBack()));
+        vehicleInventories.add(new VehicleInventory(buildMarutiHatchbackReservation(),
+                getMarutiHatchBack()));
+        return vehicleInventories;
     }
 }
---
+++ src/test/java/com/gb/rental/service/VehicleSearchTest.java
@@ -0,0 +1,58 @@
+package com.gb.rental.service;
+
+import com.gb.rental.TestData;
+import com.gb.rental.model.vehicle.HireableVehicle;
+import com.gb.rental.model.vehicle.VehicleType;
+import com.gb.rental.repository.VehicleInventoryRepository;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class VehicleSearchTest {
+    @Test
+    public void ShouldFindVehicleByType() {
+        VehicleInventoryRepository.vehicleInventoryList = TestData.buildVehicleInventory();
+        VehicleSearchService vehicleSearchService = new VehicleSearchServiceImpl();
+        List<HireableVehicle> vehicleList = vehicleSearchService.searchByType(VehicleType.HATCHBACK,
+                "Bangalore", LocalDateTime.now().plusDays(3), LocalDateTime.now().plusDays(4));
+        assertEquals(2, vehicleList.size());
+    }
+
+    @Test
+    public void ShouldFindOneVehicleByType() {
+        VehicleInventoryRepository.vehicleInventoryList = TestData.buildVehicleInventory();
+        VehicleInventoryRepository.vehicleInventoryList.get(0)
+                .setFromDate(LocalDateTime.now());
+        VehicleInventoryRepository.vehicleInventoryList.get(0)
+                .setDueDate(LocalDateTime.now().plusDays(4));
+
+        VehicleSearchService vehicleSearchService = new VehicleSearchServiceImpl();
+        List<HireableVehicle> vehicleList = vehicleSearchService.searchByType(VehicleType.HATCHBACK,
+                "Bangalore", LocalDateTime.now().plusDays(2), LocalDateTime.now().plusDays(3));
+        assertEquals(1, vehicleList.size());
+    }
+
+    @Test
+    public void ShouldFindNoVehicleByType() {
+
+        VehicleInventoryRepository.vehicleInventoryList = TestData.buildVehicleInventory();
+        VehicleInventoryRepository.vehicleInventoryList.get(0)
+                .setFromDate(LocalDateTime.now().minusDays(1));
+        VehicleInventoryRepository.vehicleInventoryList.get(0)
+                .setDueDate(LocalDateTime.now().plusDays(4));
+
+        VehicleInventoryRepository.vehicleInventoryList = TestData.buildVehicleInventory();
+        VehicleInventoryRepository.vehicleInventoryList.get(1)
+                .setFromDate(LocalDateTime.now().minusHours(5));
+        VehicleInventoryRepository.vehicleInventoryList.get(1)
+                .setDueDate(LocalDateTime.now().plusDays(2));
+
+        VehicleSearchService vehicleSearchService = new VehicleSearchServiceImpl();
+        List<HireableVehicle> vehicleList = vehicleSearchService.searchByType(VehicleType.HATCHBACK,
+                "Bangalore", LocalDateTime.now().plusDays(1), LocalDateTime.now().plusDays(3));
+        assertEquals(0, vehicleList.size());
+    }
+}
