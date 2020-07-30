fe5dc47a2f7be210feda5cfa79d54d5ec354d05a
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-07-31 00:46:38 +0530

    added logic to search for vehicles in inventory

--- src/main/java/com/gb/rental/service/VehicleSearchServiceImpl.java
+++ src/main/java/com/gb/rental/service/VehicleSearchServiceImpl.java
@@ -2,28 +2,63 @@ package com.gb.rental.service;
 
 import com.gb.rental.model.vehicle.HireableVehicle;
 import com.gb.rental.model.vehicle.VehicleType;
-import com.gb.rental.repository.VehicleRepository;
+import com.gb.rental.repository.VehicleInventoryRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class VehicleSearchServiceImpl implements VehicleSearchService {
-    VehicleRepository vehicleRepository = new VehicleRepository();
+
     @Override
     public List<HireableVehicle> searchByType(VehicleType vehicleType, String city,
                                               LocalDateTime fromDate, LocalDateTime toDate) {
-        return null;
+        List<HireableVehicle> hireableVehicles =
+                VehicleInventoryRepository.vehicleInventoryList
+                        .stream()
+                        .filter(vehicleInventory ->
+                                vehicleInventory.getVehicle().getVehicleType() == vehicleType
+                                        && vehicleInventory.getVehicle().getParkedLocation().getAddress()
+                                        .getCity().equalsIgnoreCase(city)
+                                        && fromDate.isAfter(vehicleInventory.getFromDate()) &&
+                                        toDate.isBefore(vehicleInventory.getDueDate())
+                        ).map(vehicleInventory -> vehicleInventory.getVehicle())
+                        .collect(Collectors.toList());
+        return hireableVehicles;
     }
 
     @Override
-    public List<HireableVehicle> searchByModel(String make, String city,
-                                               String model, LocalDateTime fromDate, LocalDateTime toDate) {
-        return null;
+    public List<HireableVehicle> searchByModel(String make, String model, String city,
+                                               LocalDateTime fromDate, LocalDateTime toDate) {
+        List<HireableVehicle> hireableVehicles =
+                VehicleInventoryRepository.vehicleInventoryList
+                        .stream()
+                        .filter(vehicleInventory ->
+                                vehicleInventory.getVehicle().getMake().equalsIgnoreCase(make)
+                                        && vehicleInventory.getVehicle().getModel().equalsIgnoreCase(model)
+                                        && vehicleInventory.getVehicle().getParkedLocation().getAddress()
+                                        .getCity().equalsIgnoreCase(city)
+                                        && fromDate.isAfter(vehicleInventory.getFromDate()) &&
+                                        toDate.isBefore(vehicleInventory.getDueDate())
+                        ).map(vehicleInventory -> vehicleInventory.getVehicle())
+                        .collect(Collectors.toList());
+        return hireableVehicles;
     }
 
     @Override
     public List<HireableVehicle> searchByType(int seats, String city,
                                               LocalDateTime fromDate, LocalDateTime toDate) {
-        return null;
+        List<HireableVehicle> hireableVehicles =
+                VehicleInventoryRepository.vehicleInventoryList
+                        .stream()
+                        .filter(vehicleInventory ->
+                                vehicleInventory.getVehicle().getNumberOfSeats() >= (seats)
+                                        && vehicleInventory.getVehicle().getParkedLocation().getAddress()
+                                        .getCity().equalsIgnoreCase(city)
+                                        && fromDate.isAfter(vehicleInventory.getFromDate()) &&
+                                        toDate.isBefore(vehicleInventory.getDueDate())
+                        ).map(vehicleInventory -> vehicleInventory.getVehicle())
+                        .collect(Collectors.toList());
+        return hireableVehicles;
     }
 }
